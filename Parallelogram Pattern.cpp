ccfd080e697c6da9848cbbcb244a1f76352ccd9b
AnalaVenkataSaiAmrutha <42970922+AnalaVenkataSaiAmrutha@users.noreply.github.com>
2019-07-15 00:45:49 +0530

    Add files via upload

---
+++ Parallelogram Pattern.cpp
@@ -0,0 +1,26 @@
+/*Parallelogram Pattern */
+#include<stdio.h>
+int main()
+{
+	int n,i,j,k;
+	printf("Enter number of rows:");
+	scanf("%d",&n);
+	for(i=1;i<=n;i++)
+	{
+		for(k=n;k>i;k--)
+		{
+			printf(" ");
+		}
+		for(j=1;j<=n;j++)
+		{
+			if((i==1)||(i==n)||(j==1)||(j==n))
+			printf("*");
+			else
+			printf(" ");
+			
+		}
+		printf("\n");
+	}
+	return 0;
+}
+
