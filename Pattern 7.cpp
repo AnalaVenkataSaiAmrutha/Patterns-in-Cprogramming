56232bf40baebc5f60def47714747cc8f2d941dc
AnalaVenkataSaiAmrutha <42970922+AnalaVenkataSaiAmrutha@users.noreply.github.com>
2019-07-15 15:47:46 +0530

    Add files via upload

---
+++ Pattern 7.cpp
@@ -0,0 +1,24 @@
+/* Pattern 7
+5 5 5 5 5
+4 4 4 4 4
+3 3 3 3 3
+2 2 2 2 2
+1 1 1 1 1
+ */
+#include<stdio.h> 
+int main()
+{
+	int i,j,n;
+	printf("Enter number of rows:");
+	scanf("%d",&n);
+	for(i=n;i>0;i--)
+	{
+		for(j=1;j<=n;j++)
+		{
+			printf("%d",i);
+		}
+		printf("\n");
+		
+	}
+	return 0;
+}
